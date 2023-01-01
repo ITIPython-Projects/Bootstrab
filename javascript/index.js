856a9e30ba6c554285a6ac0d0523732794a1dc36
Ahmed-AbdElRhman <abdalrhmanahmed469@gmail.com>
2023-01-01 19:53:42 +0200

    Add Recommended Proudects Slider

--- javascript/index.js
+++ javascript/index.js
@@ -6,12 +6,12 @@ var splide = new Splide('.splide', {
     breakpoints: {
         640: {
             perPage: 2,
-            gap: '.7rem',
+            gap: '.3rem',
             height: '20rem',
         },
         480: {
             perPage: 1,
-            gap: '.7rem',
+            gap: '.3rem',
             height: '20rem',
         },
     },
